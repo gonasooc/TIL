3b1aef3964f5855c687675bc3ab7f8e094435130
gonasooc <doupler@gmail.com>
2023-06-06 11:53:03 +0900

    마인드맵 과제

---
+++ assets/mudium_v1.mm
@@ -0,0 +1,216 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1686013303337" ID="ID_890867243" MODIFIED="1686019495734" STYLE="fork" TEXT="Mudium">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686014048699" ID="ID_1317279196" MODIFIED="1686019509938" POSITION="right" TEXT="front">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686015021024" ID="ID_424969990" MODIFIED="1686019509938" TEXT="&#xd68c;&#xc6d0;&#xac00;&#xc785;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686015026912" ID="ID_272387239" MODIFIED="1686019509938" TEXT="&#xb85c;&#xadf8;&#xc778;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686017568569" ID="ID_1713230573" MODIFIED="1686019509938" TEXT="&#xc0c8;&#xc18c;&#xc2dd;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017581736" ID="ID_1697044212" MODIFIED="1686019509938" TEXT="&#xd314;&#xb85c;&#xc6b0;&#xae30;&#xbc18;&#xc0c8;&#xc18c;&#xc2dd;&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1686014424786" ID="ID_1552918103" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686014548977" ID="ID_140215331" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686014772809" ID="ID_716936034" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686016701971" ID="ID_1304283704" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xc18c;&#xac1c;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686014795520" ID="ID_1781433822" MODIFIED="1686019509938" TEXT="&#xd314;&#xb85c;&#xc6b0;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686014431873" ID="ID_664791841" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xc568;&#xbc94;&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686014842416" ID="ID_579979530" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xc568;&#xbc94;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686014847016" ID="ID_1465498918" MODIFIED="1686019509938" TEXT="&#xc88b;&#xc544;&#xc694;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1686014436729" ID="ID_1532032747" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xacf5;&#xc5f0;&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686014851696" ID="ID_704156041" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xacf5;&#xc5f0;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686014860271" ID="ID_1133335" MODIFIED="1686019509938" TEXT="&#xc88b;&#xc544;&#xc694;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1686016842667" ID="ID_1639449763" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686016850955" ID="ID_231889241" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686016854530" ID="ID_67697873" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686016921747" ID="ID_381728260" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xc815;&#xbcf4;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686016858659" ID="ID_880065685" MODIFIED="1686019509938" TEXT="&#xc88b;&#xc544;&#xc694;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686016900842" ID="ID_498566734" MODIFIED="1686019509938" TEXT="&#xc608;&#xb9e4;&#xd558;&#xae30;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686016970011" ID="ID_1587119501" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc720;&#xd558;&#xae30;(&#xd074;&#xb9bd;&#xbcf4;&#xb4dc;&#xbcf5;&#xc0ac;, SNS)">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1686017148618" ID="ID_54838369" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xce98;&#xb9b0;&#xb354;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1686014704249" ID="ID_791983635" MODIFIED="1686019509938" TEXT="&#xac80;&#xc0c9;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686014716370" ID="ID_359560572" MODIFIED="1686019509938" TEXT="&#xac80;&#xc0c9;&#xacb0;&#xacfc;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1686015703189" ID="ID_501641153" MODIFIED="1686019509938" TEXT="&#xb0b4;&#xb77c;&#xc774;&#xbe0c;&#xb7ec;&#xb9ac;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686015827286" ID="ID_1769723784" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;(&#xd314;&#xb85c;&#xc789;)">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686015844861" ID="ID_200526233" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1686015808765" ID="ID_885457813" MODIFIED="1686019509938" TEXT="&#xc568;&#xbc94;(&#xc88b;&#xc544;&#xc694;)">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686015840325" ID="ID_1896645117" MODIFIED="1686019509938" TEXT="&#xc568;&#xbc94;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1686016319116" ID="ID_1814652535" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;(&#xc88b;&#xc544;&#xc694;)">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686016328827" ID="ID_589533435" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1686015794061" ID="ID_257745854" MODIFIED="1686019509938" TEXT="&#xb0b4;&#xc815;&#xbcf4;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686015797158" ID="ID_1419991820" MODIFIED="1686019509938" TEXT="&#xb0b4;&#xc815;&#xbcf4;&#xc218;&#xc815;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686018056088" ID="ID_166796284" MODIFIED="1686019509938" TEXT="Q&amp;A&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686018070712" ID="ID_235373854" MODIFIED="1686019509938" TEXT="&#xc9c8;&#xbb38;&#xb4f1;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686018088752" ID="ID_961737925" MODIFIED="1686019509938" TEXT="&#xc9c8;&#xbb38;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1686017953503" ID="ID_1671722640" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc9c0;&#xc0ac;&#xd56d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686017970335" ID="ID_1848895148" MODIFIED="1686019509938" TEXT="FAQ">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1686014059491" ID="ID_830060482" MODIFIED="1686019500693" POSITION="left" TEXT="admin">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017334505" ID="ID_1423036405" MODIFIED="1686019509938" TEXT="&#xd68c;&#xc6d0;&#xad00;&#xb9ac;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017344434" ID="ID_1610597865" MODIFIED="1686019509938" TEXT="&#xd68c;&#xc6d0;&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017779424" ID="ID_1836491531" MODIFIED="1686019509938" TEXT="&#xd68c;&#xc6d0;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017801160" ID="ID_57316068" MODIFIED="1686019509938" TEXT="&#xd68c;&#xc6d0;&#xc815;&#xbcf4;&#xc218;&#xc815;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686017813136" ID="ID_1513859841" MODIFIED="1686019509938" TEXT="&#xd68c;&#xc6d0;&#xc0ad;&#xc81c;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1686017730968" ID="ID_1942338077" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xad00;&#xb9ac;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017735632" ID="ID_1009570965" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017827568" ID="ID_827900522" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xcd94;&#xac00;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686017841663" ID="ID_763602272" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017855865" ID="ID_1799213600" MODIFIED="1686019509938" TEXT="&#xbba4;&#xc9c0;&#xc158;&#xc815;&#xbcf4;&#xc218;&#xc815;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1686017888384" ID="ID_967182190" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xad00;&#xb9ac;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017892319" ID="ID_1008562171" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017900736" ID="ID_423773093" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xcd94;&#xac00;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686017904431" ID="ID_1657046641" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017908632" ID="ID_1912130059" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc5f0;&#xc815;&#xbcf4;&#xc218;&#xc815;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1686017982111" ID="ID_1978589105" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc9c0;&#xc0ac;&#xd56d;&#xad00;&#xb9ac;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017986399" ID="ID_470133847" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc9c0;&#xc0ac;&#xd56d;&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017991568" ID="ID_482564368" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc9c0;&#xc0ac;&#xd56d;&#xcd94;&#xac00;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686017994775" ID="ID_1740855517" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc9c0;&#xc0ac;&#xd56d;&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686017998928" ID="ID_114226719" MODIFIED="1686019509938" TEXT="&#xacf5;&#xc9c0;&#xc0ac;&#xd56d;&#xc218;&#xc815;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1686018008048" ID="ID_1735934559" MODIFIED="1686019509938" TEXT=" FAQ">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686018020863" ID="ID_255454572" MODIFIED="1686019509938" TEXT="FAQ&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686018024575" ID="ID_215742407" MODIFIED="1686019509933" TEXT="FAQ&#xcd94;&#xac00;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+<node CREATED="1686018028552" ID="ID_437571518" MODIFIED="1686019509933" TEXT="FAQ&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686018033143" ID="ID_1984821975" MODIFIED="1686019509933" TEXT="FAQ&#xc218;&#xc815;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1686018106495" ID="ID_678601027" MODIFIED="1686019509933" TEXT="Q&amp;A">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686018118072" ID="ID_723130272" MODIFIED="1686019509933" TEXT="Q&amp;A&#xbaa9;&#xb85d;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686018125392" ID="ID_1142458452" MODIFIED="1686019509933" TEXT="Q&amp;A&#xc0c1;&#xc138;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+<node CREATED="1686018133352" ID="ID_1178004253" MODIFIED="1686019509918" TEXT="&#xb2f5;&#xbcc0;&#xb4f1;&#xb85d;&#xd558;&#xae30;">
+<font NAME="Pretendard Variable" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
